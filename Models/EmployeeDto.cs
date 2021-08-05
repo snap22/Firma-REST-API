5447d08e233838561f19ba60a87a5a7c5e2e9006
snap22 <39875321+snap22@users.noreply.github.com>
2021-08-05 15:03:07 +0200

    Created DTO for employee

---
+++ Models/EmployeeDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaRest.Models
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public int? CompanyId { get; set; }
+    }
+}
