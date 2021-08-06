6899e73c089d0802100204d10dc084d6a2e1ec8a
snap22 <39875321+snap22@users.noreply.github.com>
2021-08-06 11:24:01 +0200

    Created profile for mapper

---
+++ MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using FirmaRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaRest
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Employee, EmployeeDto>();
+            CreateMap<EmployeeDto, Employee>().ForMember(x => x.Id, opt => opt.Ignore());
+
+            CreateMap<Company, CompanyDto>();
+            CreateMap<CompanyDto, Company>().ForMember(x => x.Id, opt => opt.Ignore());
+
+            //CreateMap<Division, DivisionDto>();
+            //CreateMap<DivisionDto, Division>().ForMember(x => x.Id, opt => opt.Ignore());
+
+            //CreateMap<Project, ProjectDto>();
+            //CreateMap<ProjectDto, Project>().ForMember(x => x.Id, opt => opt.Ignore());
+
+            //CreateMap<Department, DepartmentDto>();
+            //CreateMap<DepartmentDto, Department>().ForMember(x => x.Id, opt => opt.Ignore());
+
+        }
+    }
+}
