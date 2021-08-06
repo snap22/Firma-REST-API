8ff2a8a270ed67c55625285c3b85d1de71db2938
snap22 <39875321+snap22@users.noreply.github.com>
2021-08-06 16:29:10 +0200

    Created repository for nodes

--- Repository/INodeRepository.cs
+++ Repository/INodeRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FirmaRest.Models;
+using FirmaRest.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,33 @@ namespace FirmaRest.Repository
 {
     interface INodeRepository
     {
+        public Task<ActionResult<IEnumerable<CompanyDto>>> GetAllCompanies();
+        public Task<ActionResult<CompanyDto>> GetCompanyById(int id);
+        public Task<ActionResult<CompanyDto>> CreateCompany(CompanyDto companyDto);
+        public Task<ActionResult<CompanyDto>> UpdateCompany(int id, CompanyDto companyDto);
+        public Task<ActionResult<CompanyDto>> DeleteCompany(int id);
+        public Task<ActionResult<IEnumerable<EmployeeDto>>> GetAllEmployeesInCompany(int companyId);
+
+        public Task<ActionResult<IEnumerable<DivisionDto>>> GetAllDivisions();
+        public Task<ActionResult<DivisionDto>> GetDivisionById(int id);
+        public Task<ActionResult<DivisionDto>> CreateDivision(DivisionDto divisionDto);
+        public Task<ActionResult<DivisionDto>> UpdateDivision(int id, DivisionDto divisionDto);
+        public Task<ActionResult<DivisionDto>> DeleteDivision(int id);
+        public Task<ActionResult<IEnumerable<DivisionDto>>> GetAllDivisionsInCompany(int companyId);
+
+        public Task<ActionResult<IEnumerable<ProjectDto>>> GetAllProjects();
+        public Task<ActionResult<ProjectDto>> GetProjectById(int id);
+        public Task<ActionResult<ProjectDto>> CreateProject(ProjectDto projectDto);
+        public Task<ActionResult<ProjectDto>> UpdateProject(int id, ProjectDto projectDto);
+        public Task<ActionResult<ProjectDto>> DeleteProject(int id);
+        public Task<ActionResult<IEnumerable<ProjectDto>>> GetAllProjectsInDivision(int divisionId);
+
+        public Task<ActionResult<IEnumerable<DepartmentDto>>> GetAllDepartments();
+        public Task<ActionResult<DepartmentDto>> GetDepartmentById(int id);
+        public Task<ActionResult<DepartmentDto>> CreateDepartment(DepartmentDto departmentDto);
+        public Task<ActionResult<DepartmentDto>> UpdateDepartment(int id, DepartmentDto departmentDto);
+        public Task<ActionResult<DepartmentDto>> DeleteDepartment(int id);
+        public Task<ActionResult<IEnumerable<DepartmentDto>>> GetAllDepartmentsInProject(int projectId);
     }
+
 }
---
+++ Repository/NodeRepository.cs
@@ -0,0 +1,113 @@
+﻿using FirmaRest.Models;
+using FirmaRest.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaRest.Repository
+{
+    public class NodeRepository : INodeRepository
+    {
+        public Task<ActionResult<IEnumerable<CompanyDto>>> GetAllCompanies()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<CompanyDto>> GetCompanyById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<CompanyDto>> CreateCompany(CompanyDto companyDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<CompanyDto>> UpdateCompany(int id, CompanyDto companyDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<CompanyDto>> DeleteCompany(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<IEnumerable<EmployeeDto>>> GetAllEmployeesInCompany(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<IEnumerable<DivisionDto>>> GetAllDivisions()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<DivisionDto>> GetDivisionById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<DivisionDto>> CreateDivision(DivisionDto divisionDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<DivisionDto>> UpdateDivision(int id, DivisionDto divisionDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<DivisionDto>> DeleteDivision(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<IEnumerable<DivisionDto>>> GetAllDivisionsInCompany(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<IEnumerable<ProjectDto>>> GetAllProjects()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<ProjectDto>> GetProjectById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<ProjectDto>> CreateProject(ProjectDto projectDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<ProjectDto>> UpdateProject(int id, ProjectDto projectDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<ProjectDto>> DeleteProject(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<IEnumerable<ProjectDto>>> GetAllProjectsInDivision(int divisionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActionResult<IEnumerable<DepartmentDto>>> GetAllDepartments()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<DepartmentDto>> GetDepartmentById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<DepartmentDto>> CreateDepartment(DepartmentDto departmentDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<DepartmentDto>> UpdateDepartment(int id, DepartmentDto departmentDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<DepartmentDto>> DeleteDepartment(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ActionResult<IEnumerable<DepartmentDto>>> GetAllDepartmentsInProject(int projectId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
