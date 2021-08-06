29eba45ff3c701a72716d3c7ba7214497e5351bf
snap22 <39875321+snap22@users.noreply.github.com>
2021-08-06 16:29:23 +0200

    Added other nodes to mapping

--- MappingProfile.cs
+++ MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using FirmaRest.Models;
+using FirmaRest.Models.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,14 +18,14 @@ namespace FirmaRest
             CreateMap<Company, CompanyDto>();
             CreateMap<CompanyDto, Company>().ForMember(x => x.Id, opt => opt.Ignore());
 
-            //CreateMap<Division, DivisionDto>();
-            //CreateMap<DivisionDto, Division>().ForMember(x => x.Id, opt => opt.Ignore());
+            CreateMap<Division, DivisionDto>();
+            CreateMap<DivisionDto, Division>().ForMember(x => x.Id, opt => opt.Ignore());
 
-            //CreateMap<Project, ProjectDto>();
-            //CreateMap<ProjectDto, Project>().ForMember(x => x.Id, opt => opt.Ignore());
+            CreateMap<Project, ProjectDto>();
+            CreateMap<ProjectDto, Project>().ForMember(x => x.Id, opt => opt.Ignore());
 
-            //CreateMap<Department, DepartmentDto>();
-            //CreateMap<DepartmentDto, Department>().ForMember(x => x.Id, opt => opt.Ignore());
+            CreateMap<Department, DepartmentDto>();
+            CreateMap<DepartmentDto, Department>().ForMember(x => x.Id, opt => opt.Ignore());
 
         }
     }
