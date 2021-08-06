3983bbfd3d8c5c7e279e0baa3e5a34242de7e8bc
snap22 <39875321+snap22@users.noreply.github.com>
2021-08-06 17:10:25 +0200

    Updated NodeRepository

--- Repository/NodeRepository.cs
+++ Repository/NodeRepository.cs
@@ -1,6 +1,8 @@
-﻿using FirmaRest.Models;
+﻿using AutoMapper;
+using FirmaRest.Models;
 using FirmaRest.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,104 +12,212 @@ namespace FirmaRest.Repository
 {
     public class NodeRepository : INodeRepository
     {
-        public Task<ActionResult<IEnumerable<CompanyDto>>> GetAllCompanies()
+        private readonly TestDBContext _context;
+        private readonly IMapper _mapper;
+        public NodeRepository(TestDBContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
-        }
-        public Task<ActionResult<CompanyDto>> GetCompanyById(int id)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<ActionResult<CompanyDto>> CreateCompany(CompanyDto companyDto)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<ActionResult<CompanyDto>> UpdateCompany(int id, CompanyDto companyDto)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<ActionResult<CompanyDto>> DeleteCompany(int id)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<ActionResult<IEnumerable<EmployeeDto>>> GetAllEmployeesInCompany(int companyId)
-        {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
         }
 
-        public Task<ActionResult<IEnumerable<DivisionDto>>> GetAllDivisions()
+
+
+        // Company
+
+        public async Task<ActionResult<IEnumerable<CompanyDto>>> GetAllCompanies()
         {
-            throw new NotImplementedException();
+            return await _context.Companies
+                .Select(c => _mapper.Map<CompanyDto>(c))
+                .ToListAsync();
         }
-        public Task<ActionResult<DivisionDto>> GetDivisionById(int id)
+        public async Task<ActionResult<CompanyDto>> GetCompanyById(int id)
         {
-            throw new NotImplementedException();
+            var company = await _context.Companies.FindAsync(id);
+            return _mapper.Map<CompanyDto>(company);
         }
-        public Task<ActionResult<DivisionDto>> CreateDivision(DivisionDto divisionDto)
+        public async Task<ActionResult<CompanyDto>> CreateCompany(CompanyDto companyDto)
         {
-            throw new NotImplementedException();
+            var company = _mapper.Map<Company>(companyDto);
+
+            _context.Companies.Add(company);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<CompanyDto>(company);
         }
-        public Task<ActionResult<DivisionDto>> UpdateDivision(int id, DivisionDto divisionDto)
+        public async Task<ActionResult<CompanyDto>> UpdateCompany(int id, CompanyDto companyDto)
         {
-            throw new NotImplementedException();
+            var company = await _context.Companies.FindAsync(id);
+
+            _mapper.Map(companyDto, company);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<CompanyDto>(company);
         }
-        public Task<ActionResult<DivisionDto>> DeleteDivision(int id)
+        public async Task<ActionResult<CompanyDto>> DeleteCompany(int id)
         {
-            throw new NotImplementedException();
+            var company = await _context.Companies.FindAsync(id);
+
+            _context.Companies.Remove(company);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<CompanyDto>(company);
         }
-        public Task<ActionResult<IEnumerable<DivisionDto>>> GetAllDivisionsInCompany(int companyId)
+        public async Task<ActionResult<IEnumerable<EmployeeDto>>> GetAllEmployeesInCompany(int companyId)
         {
-            throw new NotImplementedException();
+            return await _context.Employees
+                .Where(e => e.CompanyId == companyId)
+                .Select(e => _mapper.Map<EmployeeDto>(e))
+                .ToListAsync();
         }
 
-        public Task<ActionResult<IEnumerable<ProjectDto>>> GetAllProjects()
+
+
+        // Division
+
+        public async Task<ActionResult<IEnumerable<DivisionDto>>> GetAllDivisions()
         {
-            throw new NotImplementedException();
+            return await _context.Divisions
+                .Select(d => _mapper.Map<DivisionDto>(d))
+                .ToListAsync();
         }
-        public Task<ActionResult<ProjectDto>> GetProjectById(int id)
+        public async Task<ActionResult<DivisionDto>> GetDivisionById(int id)
         {
-            throw new NotImplementedException();
+            var division = await _context.Divisions.FindAsync(id);
+            return _mapper.Map<DivisionDto>(division);
         }
-        public Task<ActionResult<ProjectDto>> CreateProject(ProjectDto projectDto)
+        public async Task<ActionResult<DivisionDto>> CreateDivision(DivisionDto divisionDto)
         {
-            throw new NotImplementedException();
+            var division = _mapper.Map<Division>(divisionDto);
+
+            _context.Divisions.Add(division);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<DivisionDto>(division);
         }
-        public Task<ActionResult<ProjectDto>> UpdateProject(int id, ProjectDto projectDto)
+        public async Task<ActionResult<DivisionDto>> UpdateDivision(int id, DivisionDto divisionDto)
         {
-            throw new NotImplementedException();
+            var division = await _context.Divisions.FindAsync(id);
+
+            _mapper.Map(divisionDto, division);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<DivisionDto>(division);
         }
-        public Task<ActionResult<ProjectDto>> DeleteProject(int id)
+        public async Task<ActionResult<DivisionDto>> DeleteDivision(int id)
         {
-            throw new NotImplementedException();
+            var division = await _context.Divisions.FindAsync(id);
+
+            _context.Divisions.Remove(division);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<DivisionDto>(division);
         }
-        public Task<ActionResult<IEnumerable<ProjectDto>>> GetAllProjectsInDivision(int divisionId)
+        public async Task<ActionResult<IEnumerable<DivisionDto>>> GetAllDivisionsInCompany(int companyId)
         {
-            throw new NotImplementedException();
+            return await _context.Divisions
+                .Where(d => d.CompanyId == companyId)
+                .Select(d => _mapper.Map<DivisionDto>(d))
+                .ToListAsync();
         }
 
-        public Task<ActionResult<IEnumerable<DepartmentDto>>> GetAllDepartments()
+
+
+        // Project
+
+        public async Task<ActionResult<IEnumerable<ProjectDto>>> GetAllProjects()
         {
-            throw new NotImplementedException();
+            return await _context.Projects
+                .Select(p => _mapper.Map<ProjectDto>(p))
+                .ToListAsync();
         }
-        public Task<ActionResult<DepartmentDto>> GetDepartmentById(int id)
+        public async Task<ActionResult<ProjectDto>> GetProjectById(int id)
         {
-            throw new NotImplementedException();
+            var project = await _context.Projects.FindAsync(id);
+            return _mapper.Map<ProjectDto>(project);
         }
-        public Task<ActionResult<DepartmentDto>> CreateDepartment(DepartmentDto departmentDto)
+        public async Task<ActionResult<ProjectDto>> CreateProject(ProjectDto projectDto)
         {
-            throw new NotImplementedException();
+            var project = _mapper.Map<Project>(projectDto);
+
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<ProjectDto>(project);
         }
-        public Task<ActionResult<DepartmentDto>> UpdateDepartment(int id, DepartmentDto departmentDto)
+        public async Task<ActionResult<ProjectDto>> UpdateProject(int id, ProjectDto projectDto)
         {
-            throw new NotImplementedException();
+            var project = await _context.Projects.FindAsync(id);
+
+            _mapper.Map(projectDto, project);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<ProjectDto>(project);
         }
-        public Task<ActionResult<DepartmentDto>> DeleteDepartment(int id)
+        public async Task<ActionResult<ProjectDto>> DeleteProject(int id)
         {
-            throw new NotImplementedException();
+            var project = await _context.Projects.FindAsync(id);
+
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<ProjectDto>(project);
         }
-        public Task<ActionResult<IEnumerable<DepartmentDto>>> GetAllDepartmentsInProject(int projectId)
+        public async Task<ActionResult<IEnumerable<ProjectDto>>> GetAllProjectsInDivision(int divisionId)
         {
-            throw new NotImplementedException();
+            return await _context.Projects
+                .Where(p => p.DivisionId == divisionId)
+                .Select(p => _mapper.Map<ProjectDto>(p))
+                .ToListAsync();
+        }
+
+
+
+        // Department
+
+        public async Task<ActionResult<IEnumerable<DepartmentDto>>> GetAllDepartments()
+        {
+            return await _context.Departments
+                .Select(d => _mapper.Map<DepartmentDto>(d))
+                .ToListAsync();
+        }
+        public async Task<ActionResult<DepartmentDto>> GetDepartmentById(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+            return _mapper.Map<DepartmentDto>(department);
+        }
+        public async Task<ActionResult<DepartmentDto>> CreateDepartment(DepartmentDto departmentDto)
+        {
+            var department = _mapper.Map<Department>(departmentDto);
+
+            _context.Departments.Add(department);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<DepartmentDto>(department);
+        }
+        public async Task<ActionResult<DepartmentDto>> UpdateDepartment(int id, DepartmentDto departmentDto)
+        {
+            var department = await _context.Departments.FindAsync(id);
+
+            _mapper.Map(departmentDto, department);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<DepartmentDto>(department);
+        }
+        public async Task<ActionResult<DepartmentDto>> DeleteDepartment(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+
+            _context.Departments.Remove(department);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<DepartmentDto>(department);
+        }
+        public async Task<ActionResult<IEnumerable<DepartmentDto>>> GetAllDepartmentsInProject(int projectId)
+        {
+            return await _context.Departments
+                .Where(d => d.ProjectId == projectId)
+                .Select(d => _mapper.Map<DepartmentDto>(d))
+                .ToListAsync();
         }
     }
 }
