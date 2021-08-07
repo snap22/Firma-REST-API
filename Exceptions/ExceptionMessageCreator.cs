a3c961e7ec6f3485585a0b77917c60b33c166432
snap22 <39875321+snap22@users.noreply.github.com>
2021-08-07 14:15:50 +0200

    created a class for creating exception messages

---
+++ Exceptions/ExceptionMessageCreator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaRest.Exceptions
+{
+    public class ExceptionMessageCreator
+    {
+        public string CreateNotExistsMessage(string name, int id)
+        {
+            return string.Format("{0} with id: {1} does not exist!", name, id);
+        }
+
+        public string CreateEmployeeNotExistsMessage(int employeeId)
+        {
+            return CreateNotExistsMessage("Employee", employeeId);
+        }
+
+        public string CreateCompanyNotExistsMessage(int companyId)
+        {
+            return CreateNotExistsMessage("Company", companyId);
+        }
+
+        public string CreateDivisionNotExistsMessage(int divisionId)
+        {
+            return CreateNotExistsMessage("Division", divisionId);
+        }
+
+        public string CreateProjectNotExistsMessage(int projectId)
+        {
+            return CreateNotExistsMessage("Project", projectId);
+        }
+
+        public string CreateDepartmentNotExistsMessage(int departmentId)
+        {
+            return CreateNotExistsMessage("Department", departmentId);
+        }
+
+        public string CreateEmployeeDifferentCompanyMessage()
+        {
+            return "Employee is from a different company";
+        }
+    }
+}
