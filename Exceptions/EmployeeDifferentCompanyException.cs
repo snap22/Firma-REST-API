5c7dd4e8a8bdfc97e8914fafdb486c47e6e1a305
snap22 <39875321+snap22@users.noreply.github.com>
2021-08-07 14:15:39 +0200

    Created exceptions

---
+++ Exceptions/EmployeeDifferentCompanyException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaRest.Exceptions
+{
+    public class EmployeeDifferentCompanyException : Exception
+    {
+        public EmployeeDifferentCompanyException()
+        {
+        }
+
+        public EmployeeDifferentCompanyException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Exceptions/NotExistsException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaRest.Exceptions
+{
+    public class NotExistsException : Exception
+    {
+        public NotExistsException()
+        {
+        }
+
+        public NotExistsException(string message) : base(message)
+        {
+        }
+    }
+}
