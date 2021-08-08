3d0bb5a4884a2be49c5bd33b00d0617f22813124
snap22 <39875321+snap22@users.noreply.github.com>
2021-08-08 11:41:42 +0200

    New exceptions for employee

---
+++ Exceptions/CannotDeleteException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaRest.Exceptions
+{
+    public class CannotDeleteException : Exception
+    {
+        public CannotDeleteException()
+        {
+        }
+
+        public CannotDeleteException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Exceptions/CannotModifyException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaRest.Exceptions
+{
+    public class CannotModifyException : Exception
+    {
+        public CannotModifyException()
+        {
+        }
+
+        public CannotModifyException(string message) : base(message)
+        {
+        }
+    }
+}
