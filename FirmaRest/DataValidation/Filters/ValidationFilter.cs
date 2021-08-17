62225302fb6a0c0e1607bc6d24967627cb222ecc
snap22 <39875321+snap22@users.noreply.github.com>
2021-08-17 16:43:02 +0200

    Created ValidationFilter

---
+++ FirmaRest/DataValidation/Filters/ValidationFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaRest.DataValidation.Filters
+{
+    public class ValidationFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+
+            await next();
+        }
+    }
+}
