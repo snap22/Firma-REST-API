c12a2fed3409291f953a7f73fead689cafc99231
snap22 <39875321+snap22@users.noreply.github.com>
2021-08-17 16:42:41 +0200

    Created validators for Dto models

---
+++ FirmaRest/DataValidation/Validators/EmployeeValidator.cs
@@ -0,0 +1,38 @@
+﻿using FirmaRest.Models.DTO;
+using FluentValidation;
+
+namespace FirmaRest.DataValidation.Validators
+{
+    public class EmployeeValidator : AbstractValidator<EmployeeDto>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(e => e.FirstName)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(20)
+                .Matches("^[A-Za-z]+$");
+
+            RuleFor(e => e.LastName)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(20)
+                .Matches("^[A-Za-z]+$");
+
+            RuleFor(e => e.Title)
+                .MaximumLength(10)
+                .Matches("^[a-zA-Z]+[.]?$");
+
+            RuleFor(e => e.Email)
+                .EmailAddress()
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(e => e.Contact)
+                .NotEmpty()
+                .NotNull()
+                .Matches(@"^[+]?\d{10,}$")
+                .MaximumLength(15);
+        }
+    }
+}
---
+++ FirmaRest/DataValidation/Validators/NodesValidator.cs
@@ -0,0 +1,100 @@
+﻿using FirmaRest.Models.DTO;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaRest.DataValidation.Validators
+{
+    public class CompanyValidator : AbstractValidator<CompanyDto>
+    {
+
+        public CompanyValidator()
+        {
+            RuleFor(c => c.Title)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(20);
+
+            RuleFor(c => c.Code)
+                .Length(4)
+                .Matches("[a-zA-Z0-9,.]{4}");
+
+            RuleFor(c => c.Director)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+
+    public class DivisionValidator : AbstractValidator<DivisionDto>
+    {
+
+        public DivisionValidator()
+        {
+            RuleFor(d => d.Title)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(20);
+
+            RuleFor(d => d.Code)
+                .Length(4)
+                .Matches("[a-zA-Z0-9,.]{4}");
+
+            RuleFor(d => d.CompanyId)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(d => d.Leader)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+
+    public class ProjectValidator : AbstractValidator<ProjectDto>
+    {
+
+        public ProjectValidator()
+        {
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(20);
+
+            RuleFor(p => p.Code)
+                .Length(4)
+                .Matches("[a-zA-Z0-9,.]{4}");
+
+            RuleFor(p => p.DivisionId)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(p => p.Leader)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+
+    public class DepartmentValidator : AbstractValidator<DepartmentDto>
+    {
+        public DepartmentValidator()
+        {
+            RuleFor(d => d.Title)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(20);
+
+            RuleFor(d => d.Code)
+                .Length(4)
+                .Matches("[a-zA-Z0-9,.]{4}");
+
+            RuleFor(d => d.ProjectId)
+                .NotNull()
+                .NotEmpty();
+
+            RuleFor(d => d.Leader)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
