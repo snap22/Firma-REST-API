cbccffeb89a248001750271e2ac0fa1793a1ca41
snap22 <39875321+snap22@users.noreply.github.com>
2021-08-17 17:21:39 +0200

    created already used exception

---
+++ FirmaRest/Exceptions/AlreadyUsedException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirmaRest.Exceptions
+{
+    public class AlreadyUsedException : Exception
+    {
+        public AlreadyUsedException()
+        {
+        }
+
+        public AlreadyUsedException(string message) : base(message)
+        {
+        }
+    }
+}
